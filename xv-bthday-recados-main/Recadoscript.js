69f33a34312136fb848be610c6719596eb1f1fa5
Rafael <wizd_x@hotmail.com>
2024-09-09 13:14:03 -0300

    Final no .env

--- xv-bthday-recados-main/Recadoscript.js
+++ xv-bthday-recados-main/Recadoscript.js
@@ -3,14 +3,14 @@ import { getDatabase, ref, set, onValue } from 'firebase/database';
 import './Recadostyles.css'; 
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyDbF2NHNj4wdRiNJKrRoQ4pAoVkJAy_yP8",
+    authDomain: "xv-sophia-4ac28.firebaseapp.com",
+    databaseURL: "https://xv-sophia-4ac28-default-rtdb.firebaseio.com",
+    projectId: "xv-sophia-4ac28",
+    storageBucket: "xv-sophia-4ac28.appspot.com",
+    messagingSenderId: "636614421828",
+    appId: "1:636614421828:web:7111bcf89337fef75e7b11",
+    measurementId: "G-X8DEW4STTG"
 };
 
 // Inicializa o Firebase
